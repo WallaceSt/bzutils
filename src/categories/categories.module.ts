75445f4b7f5c102ea34cc993baf5177344a9c7bb
WallaceSt <wallacycleyton@gmail.com>
2026-01-12 15:09:59 -0300

    🧹 creanup: cleaning imports from modules

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/users/entities/users.entity';
 import { Category } from './entities/category.entity';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Category]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [TypeOrmModule, CategoriesService],
--- src/periods/periods.module.ts
+++ src/periods/periods.module.ts
@@ -3,10 +3,9 @@ import { PeriodsService } from './periods.service';
 import { PeriodsController } from './periods.controller';
 import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { Period } from './entities/period.entity';
-import { User } from 'src/users/entities/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Period])],
+  imports: [TypeOrmModule.forFeature([Period])],
   controllers: [PeriodsController],
   providers: [PeriodsService],
   exports: [PeriodsService, TypeOrmModule],
